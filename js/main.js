82e8219076394065db390906cfa5f0edaf6ff0ca
orxan666 <orxanallahyarov03@gmail.com>
2022-08-17 13:19:16 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+'use strict';
+
+
+// var a=10
+
+// var b=3
+
+// console.log(a/b);
+
+
+
+var a = "Driver"
+
+// console.log(a.indexOf("d"));
+// console.log(a[2]); eyni
+// console.log(a.charAt(2)); eyni
+// console.log(a.toUpperCase());
+// console.log(a.toLowerCase());
+
+// ----------------
+// Template Literal
+
+
+// var ad="Orxan"
+// var soyad="Esedzade"
+// var age=18
+// var salary=4000
+// var age1
+
+// // console.log("Sizin adiniz: "+ad+"\n"+"Sizin soyadiniz: "+soyad+"\n"+"sizin maas: "+salary);
+
+// console.log(`Sizin adiniz: ${ad} \nsizin soyadiniz: ${soyad} yasin: ${age1}`);
+
+
+// var sinif=["Kama","Rufo","Eli","Muro"]
+var numbers = [20, 15, 40, 50,-100, 100, 95, 77]
+// console.log(sinif[4]);
+
+// console.log(sinif.push("Ayka")); sonuna
+
+// console.log(sinif.unshift("Ayka"));evveline
+// console.log(sinif.pop());sonundan
+// console.log(sinif.shift());evvelden
+// console.log(sinif.splice(2));
+// console.log(sinif.reverse());
+// console.log(numbers.sort(function(b,a){
+//     return a-b
+// }));
+
+// console.log(sinif);
+
+
